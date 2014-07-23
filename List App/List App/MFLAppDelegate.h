7ba97e3ab7e9449da6139ebfce8781e240716150
Jo Albright <me@jo2.co>
2014-07-23 10:16:03 -0400

    create list app

---
+++ List App/List App/MFLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MFLAppDelegate.h
+//  List App
+//
+//  Created by Jo Albright on 7/23/14.
+//  Copyright (c) 2014 Jo Albright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ List App/List App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  List App
+//
+//  Created by Jo Albright on 7/23/14.
+//  Copyright (c) 2014 Jo Albright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MFLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MFLAppDelegate class]));
+    }
+}
