c73a25d06350ecfe050e8ba67ae6f3b96fe16722
Jo Albright <me@jo2.co>
2015-03-03 12:05:30 -0500

    messaging

--- PushIt/PushIt/AppDelegate.swift
+++ PushIt/PushIt/AppDelegate.swift
@@ -17,10 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
 
-//        let splitViewController = self.window!.rootViewController as UISplitViewController
-//        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as UINavigationController
-//        navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
-//        splitViewController.delegate = self
+        let splitViewController = self.window!.rootViewController as UISplitViewController
+        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as UINavigationController
+        navigationController.topViewController.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
+        splitViewController.delegate = self
         
         Parse.setApplicationId("FZ6gXhF0VcYmYwYYOl6QwsDEdDVNR1WY2cqK6mEi", clientKey: "p8H10Dz9E1VgdeNYJxvKH5ElhoGlR5zKtNzqnAH0")
         
@@ -82,8 +82,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController!, ontoPrimaryViewController primaryViewController:UIViewController!) -> Bool {
         if let secondaryAsNavController = secondaryViewController as? UINavigationController {
-            if let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController {
-                if topAsDetailController.detailItem == nil {
+            if let topAsDetailController = secondaryAsNavController.topViewController as? DetailTableViewController {
+                if topAsDetailController.user == nil {
                     // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
                     return true
                 }
