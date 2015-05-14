32fa71a77a893d01eca089c79cf2d2401adedadd
Jo Albright <me@jo2.co>
2015-05-14 09:14:34 -0400

    create app

---
+++ ScratchDraw/ScratchDraw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScratchDraw
+//
+//  Created by Jo Albright on 5/14/15.
+//  Copyright (c) 2015 Jo Albright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
