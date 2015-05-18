4e28b939bb9ca350ed0e1b0fed13352a8987894f
Jo Albright <me@jo2.co>
2015-05-18 10:18:07 -0400

    fun with shapes

--- ScratchDraw/ScratchDraw/ScratchView.swift
+++ ScratchDraw/ScratchDraw/ScratchView.swift
@@ -27,19 +27,23 @@ class ScratchView: UIView {
             
             if let firstPoint = scratch.points.first {
             
-                if let strokeColor = scratch.strokeColor {
+                switch scratch.type {
                     
-                    strokeColor.set()
+                case .Ellipse :
                     
-                    CGContextMoveToPoint(context, firstPoint.x, firstPoint.y)
+                    scratch.drawEllipseWithContext(context)
                     
-                    for point in scratch.points {
-                        
-                        CGContextAddLineToPoint(context, point.x, point.y)
-                        
-                    }
+                case .Line :
                     
-                    CGContextStrokePath(context)
+                    scratch.drawLineWithContext(context)
+                    
+                case .Rect :
+                    
+                    scratch.drawRectWithContext(context)
+                    
+                case .Triangle :
+                    
+                    scratch.drawTriangleWithContext(context)
                     
                 }
                 
@@ -54,7 +58,9 @@ class ScratchView: UIView {
         var scratch = Scratch()
         scratch.points = [point,point]
         
-        scratch.strokeColor = currentColor
+        scratch.type = .Triangle
+        
+        scratch.fillColor = currentColor
         
         scratches.append(scratch)
         setNeedsDisplay()
@@ -85,8 +91,18 @@ class ScratchView: UIView {
 
 }
 
+enum ScratchType {
+    
+    case Line
+    case Rect
+    case Ellipse
+    case Triangle
+    
+}
+
 class Scratch {
     
+    var type: ScratchType = .Line
     var points: [CGPoint] = []
     var fillColor: UIColor?
     var strokeColor: UIColor?
@@ -96,6 +112,87 @@ class Scratch {
     
     // line cap
     // line join
+    
+    func drawLineWithContext(context: CGContextRef) {
+        
+        if let fillColor = fillColor {
+            
+            fillColor.set()
+            
+            CGContextMoveToPoint(context, points[0].x, points[0].y)
+            
+            for point in points {
+                
+                CGContextAddLineToPoint(context, point.x, point.y)
+                
+            }
+            
+            CGContextFillPath(context)
+            
+        }
+        
+    }
+    
+    func drawEllipseWithContext(context: CGContextRef) {
+        
+        if let fillColor = fillColor {
+            
+            fillColor.set()
+            
+            let x = points[0].x
+            let y = points[0].y
+            let width = points[1].x - points[0].x
+            let height = points[1].y - points[0].y
+            
+            let rect = CGRectMake(x, y, width, height)
+            
+            CGContextFillEllipseInRect(context, rect)
+            
+        }
+        
+    }
+    
+    func drawRectWithContext(context: CGContextRef) {
+        
+        if let fillColor = fillColor {
+            
+            fillColor.set()
+            
+            let x = points[0].x
+            let y = points[0].y
+            let width = points[1].x - points[0].x
+            let height = points[1].y - points[0].y
+            
+            let rect = CGRectMake(x, y, width, height)
+            
+            CGContextFillRect(context, rect)
+            
+        }
+        
+    }
+    
+    func drawTriangleWithContext(context: CGContextRef) {
+        
+        if let fillColor = fillColor {
+            
+            fillColor.set()
+            
+            CGContextMoveToPoint(context, points[1].x, points[1].y)
+            
+            CGContextAddLineToPoint(context, points[0].x, points[1].y)
+            
+            let midX = (points[0].x + points[1].x) / 2.0
+            
+            CGContextAddLineToPoint(context, midX, points[0].y)
+            
+            // will automatically fill in line from last point to first point
+            CGContextAddLineToPoint(context, points[1].x, points[1].y)
+            
+            CGContextFillPath(context)
+            
+        }
+        
+    }
     
 }
 
--- ScratchDraw/ScratchDraw/ViewController.swift
+++ ScratchDraw/ScratchDraw/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var fillColors: [UIColor] = [
+    UIColor.magentaColor(),
+    UIColor.cyanColor(),
+    UIColor.yellowColor(),
+    UIColor.blackColor()
+]
 
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+
+    @IBOutlet weak var viewBottomConstraint: NSLayoutConstraint!
+    
+    
+    @IBOutlet weak var fillColorCollectionView: UICollectionView!
+    
     @IBOutlet weak var scratchPad: ScratchView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        view.addSubview(scratchPad)
-//        scratchPad.frame = view.frame
-//        scratchPad.backgroundColor = UIColor.whiteColor()
+        fillColorCollectionView.dataSource = self
+        fillColorCollectionView.delegate = self
+        
+        viewBottomConstraint.constant = -300
         
     }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return fillColors.count
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("colorCell", forIndexPath: indexPath) as! ColorCell
+        
+        cell.backgroundColor = fillColors[indexPath.item]
+        
+        return cell
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        let cell = collectionView.cellForItemAtIndexPath(indexPath) as! ColorCell
+        
+        if let color = cell.backgroundColor {
+            
+            scratchPad.currentColor = color
+            
+        }
+        
+    }
+    
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         
@@ -37,18 +79,8 @@ class ViewController: UIViewController {
         if let touch = touches.first as? UITouch {
             
             let location = touch.locationInView(scratchPad)
-//            scratchPad.updateCurrentScratchWithLastPoint(location)
-            scratchPad.addPointToCurrentScratch(location)
-            
-        }
-        
-    }
-        
-    @IBAction func changeColor(sender: UIButton) {
-        
-        if let color = sender.backgroundColor {
-            
-            scratchPad.currentColor = color
+            scratchPad.updateCurrentScratchWithLastPoint(location)
+//            scratchPad.addPointToCurrentScratch(location)
             
         }
         
@@ -59,6 +91,18 @@ class ViewController: UIViewController {
         
         scratchPad.scratches = []
         scratchPad.setNeedsDisplay()
+        
+    }
+    
+    @IBAction func showHide(sender: AnyObject) {
+        
+        viewBottomConstraint.constant = (viewBottomConstraint.constant == 0) ? -300 : 0
+        
+        UIView.animateWithDuration(0.4, animations: { () -> Void in
+            
+            
+            
+        })
         
     }
     
