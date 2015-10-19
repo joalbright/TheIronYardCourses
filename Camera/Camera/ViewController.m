79d8f9a2f4483efb019f7d222dee2f76c8a2e068
Jo Albright <me@jo2.co>
2015-10-19 14:15:18 -0400

    playing with the camera

--- Camera/Camera/ViewController.m
+++ Camera/Camera/ViewController.m
@@ -8,20 +8,68 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#import <MobileCoreServices/MobileCoreServices.h>
+
+@interface ViewController () <UINavigationControllerDelegate,UIImagePickerControllerDelegate>
+
+@property UIImagePickerController * pickerController;
+
+@property (weak, nonatomic) IBOutlet UIImageView * mySelfieImageView;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    
+    self = [super initWithCoder:aDecoder];
+    
+    if (self != nil) {
+        
+        self.pickerController = [[UIImagePickerController alloc] init];
+        self.pickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+        self.pickerController.cameraDevice = UIImagePickerControllerCameraDeviceFront;
+        self.pickerController.delegate = self;
+        
+    }
+    
+    return self;
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+//    [self.view addSubview:self.pickerController.view];
+    
+    if (self.mySelfieImageView.image == nil) {
+
+        [self presentViewController:self.pickerController animated:YES completion:nil];
+
+    }
+    
+    
+}
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    NSString * type = info[UIImagePickerControllerMediaType];
+    
+    if (type == (NSString *)kUTTypeImage) {
+        
+        UIImage * image = info[UIImagePickerControllerOriginalImage];
+        self.mySelfieImageView.image = image;
+        
+    }
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+    
 }
 
 @end
+
+
+
+
+
+
