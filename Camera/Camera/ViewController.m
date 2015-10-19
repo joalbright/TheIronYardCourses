72a3294a1e2b16b2d7d4cebba2513d18d5e44bb8
Jo Albright <me@jo2.co>
2015-10-19 09:25:53 -0400

    initial create

---
+++ Camera/Camera/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Camera
+//
+//  Created by Jo Albright on 10/19/15.
+//  Copyright © 2015 Jo Albright. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
